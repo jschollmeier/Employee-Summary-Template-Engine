ea7a23c20c5330a6b8e49722ee35daf6183ba2dd
Kent Schollmeier <jschollmeier@gmail.com>
2020-07-09 14:11:44 -0500

    working program unpolished

--- Develop/app.js
+++ Develop/app.js
@@ -14,6 +14,153 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+var id = 0;
+var engineers = [];
+var interns = [];
+var employees = [];
+var manager = "";
+
+
+inquirer.prompt([
+{
+    type: "input",
+    name: "managerName",
+    message: "What is your name?"
+},
+{
+    type: "input",
+    name: "managerEmail",
+    message: "What is your email"
+},
+{
+    type: "input",
+    name: "officeNumber",
+    message: "What is your office Number?"
+}]).then(function(oput){
+    
+    employees[id] = new Manager(oput.managerName, id, oput.managerEmail, oput.officeNumber);
+    id++;
+    
+    promptFunc()})
+
+function promptFunc(){
+
+    inquirer.prompt([
+        {
+    type: "checkbox",
+    message: "Do you have any more employees?",
+    name: "moreEmployees",
+    choices: [
+        "Yes",
+        "No"
+    ] 
+    }
+
+]).then(function(data){
+
+    moreEmployees(data)})
+
+}
+function moreEmployees(data){
+    if(String(data.moreEmployees)==='Yes'){
+        inquirer.prompt([
+            {
+                type: "checkbox",
+                name: "employeeType",
+                message: "What type of employee are you?",
+                choices: [
+                    "Engineer",
+                    "Intern"
+                ]
+            }
+        ]).then(function(datas){
+            
+            newEngineer(datas)})
+    }
+    
+    else{
+        
+        var html = render(employees);
+        fs.writeFile(outputPath, html, function(err){
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log("file written")
+            }
+        })
+        
+        console.log("your employee page has been created");
+    }
+
+
+}
+
+function newEngineer(datas){
+    if(String(datas.employeeType)==='Engineer'){
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "What is the employees name?"
+            },
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "What is your employees email?"
+                
+            },
+            {
+                type: "input",
+                name: "engineerGit",
+                message: "What is your engineers github username?"
+            }
+
+        ]).then(function(ouput){
+            
+            
+            employees[id] = new Engineer(ouput.engineerName, id, ouput.engineerEmail, ouput.engineerGit);
+            console.log(employees[id].getName());
+            id++;
+            
+            
+            promptFunc()});
+
+    }
+    else{
+        newIntern();
+    }
+
+}
+function newIntern(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "internName",
+            message: "What is the employees name?"
+        },
+        {
+            type: "input",
+            name: "internEmail",
+            message: "What is your employees email?"
+            
+        },
+        {
+            type: "input",
+            name: "internSchool",
+            message: "What is the interns school?"
+        }
+        
+    ]).then(function(output){
+        
+        employees[id] = new Intern(output.internName, id, output.internEmail, output.internSchool);
+        id++;
+        
+        promptFunc()});
+}
+
+
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,27 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee.js");
+
+
+class Intern extends Employee{
+    constructor(name, id, email, school){
+        super(name, id, email);
+        this.school = school;
+    }
+
+    getSchool(){
+        return this.school;
+    }
+
+    getRole(){
+        //super.getRole();
+        return 'Intern';
+    }
+}
+
+//var interns = new Intern('Jake', 1, 'jschollmeier@gmail.com','Wisconsin');
+
+//console.log(interns.getRole())
+//console.log(interns.getSchool());
+//console.log(interns.getEmail());
+
+module.exports = Intern;
